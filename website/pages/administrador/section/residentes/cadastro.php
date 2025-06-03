25aaae8b5d1444fd9e924e6f225e1d2581c2a5a3
EnderHugo <enderhugo09@gmail.com>
2025-06-03 17:07:16 -0300

    pagina de cadastro

--- website/pages/administrador/section/residentes/cadastro.php
+++ website/pages/administrador/section/residentes/cadastro.php
@@ -1,2 +1,55 @@
-<p>CADASTRO DE RESIDENTE</p>
-<input type="text">
+<div class="cadastro">
+    <div class="card">
+        <form action="">
+            <div class="col-2">   
+                <div class="info">
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="id">Nome Completo<span>*</span></label>
+                            <input type="text"id="nome">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="nasc">Data de nascimento<span>*</span></label>
+                            <input type="date" id="nasc">
+                        </div>
+                        <div class="form-group">
+                            <label for="nasc">Naturalidade<span>*</span></label>
+                            <input type="text" id="naturalidade">
+                        </div>
+                        <div class="form-group">
+                            <label for="genero">Gênero</label>
+                            <input type="text" id="genero">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" id="email">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="telefone">Telefone</label>
+                            <input type="text" id="telefone">
+                        </div>
+                        <div class="form-group">
+                            <label for="cpf">CPF</label>
+                            <input type="text" id="cpf">
+                        </div>
+                    </div>
+                </div> 
+                <div class="documentos"></div>
+            </div> 
+            <div class="col-1">
+                <div class="form-group max-height">
+                    <label for="dados">dados</label>
+                    <br>
+                    <div class="dados" id="dados">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
